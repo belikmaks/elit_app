805d00eafa47c0f25e867e5c0eb82b9bf879e290
admin <admin@MAKSYM3A61>
2025-04-30 00:18:35 +0300

    Add project files.

---
+++ elit_app/Applicant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elit_app
+{
+    internal class Applicant
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public double GPA { get; set; }
+        public string SchoolNumber { get; set; }
+    }
+}
